6d432a33b8b1cf2232d8678d4c487ca599be777a
ToF <cthibaut@octo.com>
2017-05-25 23:14:38 +0200

    Result Price and Result Item

--- Total.hs
+++ Total.hs
@@ -9,7 +9,7 @@ type Item = (Code,(Label,Price))
 data ItemList = It Item ItemList
               | Nil
 
-data Result = Result Price    
+data Result a = Result a 
             | NoResult
     deriving (Show, Eq)
 
@@ -18,3 +18,7 @@ cash q p = fromInteger q * p
 findPrice (It (code,item) items) s | code == s = Result $ snd item
                                    | otherwise = findPrice items s 
 findPrice Nil _ = NoResult
+
+findItem (It (code,item) items) s | code == s = Result item
+                                  | otherwise = findItem items s 
+findItem Nil _ = NoResult
