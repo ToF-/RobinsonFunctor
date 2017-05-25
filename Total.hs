4f317852cd1268f316fffddb442996f607504696
ToF <cthibaut@octo.com>
2017-05-25 20:09:08 +0200

    type aliases

--- Total.hs
+++ Total.hs
@@ -1,17 +1,22 @@
 module Total
 where
 
-total :: Integer -> Double -> Double
+type Quantity = Integer
+type Price    = Double
+type Ref      = String
+
+total :: Quantity -> Price -> Price
 total q p = p * fromInteger q 
 
 data Items = Nil
-           | Item (String,Double) Items
+           | Item (Ref, Price) Items
 
+findPrice :: Items -> Ref -> Price
 findPrice (Nil) _ = 0
 findPrice (Item (r,p) is) s | r == s = p
                             | otherwise = findPrice is s 
 
-showTotal :: Double -> String
+showTotal :: Price -> String
 showTotal n = 
     let m = show $ truncate $ (n + 0.005) * 100 
         l = length m - 2
