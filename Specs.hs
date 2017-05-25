b5d41f320117b5f044000be889bd800d42fc2c65
ToF <cthibaut@octo.com>
2017-05-25 20:30:50 +0200

    Result type

--- Specs.hs
+++ Specs.hs
@@ -1,7 +1,7 @@
 import Test.Hspec
 import Total
 
-shouldBe_ a b = shouldBe (rounded a) (rounded b)
+shouldBe_ (Result a) (Result b) = shouldBe (rounded a) (rounded b)
     where
     rounded x = ((fromInteger . truncate) (x * 10000)) / 10000 
 
@@ -10,28 +10,33 @@ t = Item ("apple", 1.20) (Item ("banana", 1.34) Nil)
 main = hspec $ do
     describe "total" $ do
         it "should multiply a quantity by a price" $ do
-            total 3 1.20 `shouldBe_` 3.60
+            total 3 (Result 1.20) `shouldBe_` Result 3.60
 
     describe "showTotal" $ do
         it "should display properly the total" $ do
-            showTotal 3.00 `shouldBe` "3.00"
-            showTotal 1.00 `shouldBe` "1.00"
-            showTotal 1.24 `shouldBe` "1.24"
-            showTotal 3.02 `shouldBe` "3.02"
-            showTotal 3.02243 `shouldBe` "3.02"
-            showTotal 3.2     `shouldBe` "3.20"
-            showTotal 3.09143 `shouldBe` "3.09"
-            showTotal 3.09543 `shouldBe` "3.10" 
-            showTotal 3095.314 `shouldBe` "3095.31" 
+            showTotal (Result 3.00) `shouldBe` "3.00"
+            showTotal (Result 1.00) `shouldBe` "1.00"
+            showTotal (Result 1.24) `shouldBe` "1.24"
+            showTotal (Result 3.02) `shouldBe` "3.02"
+            showTotal (Result 3.02243) `shouldBe` "3.02"
+            showTotal (Result 3.2)     `shouldBe` "3.20"
+            showTotal (Result 3.09143) `shouldBe` "3.09"
+            showTotal (Result 3.09543) `shouldBe` "3.10" 
+            showTotal (Result 3095.314) `shouldBe` "3095.31" 
 
     describe "findPrice" $ do
         it "should find the price of an item" $ do
-            findPrice t "apple" `shouldBe` 1.20
-            findPrice t "banana" `shouldBe` 1.34
-            findPrice t "peach" `shouldBe` 0
+            findPrice t "apple" `shouldBe` Result 1.20
+            findPrice t "banana" `shouldBe` Result 1.34
+
+        it "should not be zero nor fail when item not found" $ do
+            findPrice t "peach" `shouldBe` NoResult
             
     describe "cash" $ do 
         it "given a quantity and a ref, should give a total price" $ do
             cash t "20 banana"  `shouldBe` "26.80"
             cash t "10 apple"   `shouldBe` "12.00"
+
+        it "should say when a ref was not found" $ do
+            cash t "20 peach"  `shouldBe` "??"
 
--- Total.hs
+++ Total.hs
@@ -5,25 +5,31 @@ type Quantity = Integer
 type Price    = Double
 type Ref      = String
 
-total :: Quantity -> Price -> Price
-total q p = p * fromInteger q 
+total :: Quantity -> Result -> Result
+total q p = case p of
+    Result r -> Result $ r * fromInteger q 
+    NoResult -> NoResult
 
 data Items = Nil
            | Item (Ref, Price) Items
 
-findPrice :: Items -> Ref -> Price
-findPrice (Nil) _ = 0
-findPrice (Item (r,p) is) s | r == s = p
+data Result = NoResult
+            | Result Double 
+    deriving (Eq, Show)
+
+findPrice :: Items -> Ref -> Result
+findPrice (Nil) _ = NoResult 
+findPrice (Item (r,p) is) s | r == s = Result p
                             | otherwise = findPrice is s 
 
-showTotal :: Price -> String
-showTotal n = 
+showTotal :: Result -> String
+showTotal (Result n) = 
     let m = show $ truncate $ (n + 0.005) * 100 
         l = length m - 2
     in (take l m) ++ "." ++ (drop l m) 
+showTotal NoResult = "??"
 
 cash :: Items -> String -> String
 cash t s = let [q,r] = words s in showTotal $ total (read q) (findPrice t r)
-
 
 
