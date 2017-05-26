fec6be394714993ee96e763183827aad0c1c6ff6
ToF <cthibaut@octo.com>
2017-05-26 07:29:17 +0200

    extract mapf

--- Specs.hs
+++ Specs.hs
@@ -3,17 +3,6 @@ import Total
 
 main = hspec $ do
     let items = It ("AP",("apple",1.20)) (It ("BA",("banana",1.89)) Nil)
-    describe "cash" $ do
-        it "should multiply a quantity by a price" $ do
-            cash 5 1.20 `shouldBe` 6.00
-            cash 2 3.42 `shouldBe` 6.84
-
-    describe "findPrice" $ do
-        it "should find the price of an item in a list of items" $ do
-            findPrice items "AP" `shouldBe` Result 1.20
-            findPrice items "BA" `shouldBe` Result 1.89
-        it "should not fail or give 0 when item not found" $ do
-            findPrice items "PE" `shouldBe` NoResult 
 
     describe "findItem" $ do
         it "should find the item or no result" $ do
