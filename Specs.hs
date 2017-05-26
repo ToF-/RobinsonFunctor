0194a9f2d66308d4cd6ce325c4c1a867df6e7a36
ToF <cthibaut@octo.com>
2017-05-26 14:35:11 +0200

    using List

--- Specs.hs
+++ Specs.hs
@@ -3,35 +3,10 @@ import Total
 import Data.Char
 
 main = hspec $ do
-    let items = It ("AP",("apple",1.20)) (It ("BA",("banana",1.89)) Nil)
-
-    describe "findItem" $ do
-        it "should find the item or no result" $ do
-            findItem items "AP" `shouldBe` Just ("apple",1.20)
-            findItem items "BA" `shouldBe` Just ("banana",1.89)
-            findItem items "PE" `shouldBe` Nothing 
+    let items = [("AP",("apple",1.20)), ("BA",("banana",1.89))]
 
     describe "total" $ do
         it "should return total price for a quantity and an item" $ do
             total items 10 "BA" `shouldBe` Just 18.90
         it "should not fail or give 0 when item not found" $ do
           total items 10 "PE" `shouldBe` Nothing 
-
-    describe "itemMap" $ do
-        it "should apply a function to a list of items" $ do
-            let items' = itemMap (\(c,(l,p)) -> (c,(map toUpper l,p))) items
-            items' `shouldBe` 
-              It ("AP",("APPLE",1.20)) (It ("BA",("BANANA",1.89)) Nil)
-            let items' = itemMap (\(c,(l,p)) -> (c,(l,p+0.1))) items
-            items' `shouldBe` 
-              It ("AP",("apple",1.30)) (It ("BA",("banana",1.99)) Nil)
-
-    describe "fmap" $ do
-        it "should apply a function to a function" $ do
-            let f = const 41
-                f'= fmap (+1) f
-            f' 0 `shouldBe` 42 
-            let p = (\(c,(l,p)) -> (c,(l,p+0.1)))
-                u = (\(c,(l,p)) -> (c,(map toUpper l,p)))
-                f = fmap p $ fmap u $ id
-            f ("PE",("peach",3.0)) `shouldBe` ("PE",("PEACH",3.1))
--- Total.hs
+++ Total.hs
@@ -7,21 +7,6 @@ type Label = String
 type Code  = String
 type Item = (Code,(Label,Price))
 
-data ItemList = It Item ItemList
-              | Nil
-    deriving (Show, Eq)
-
-
-total :: ItemList -> Quantity -> Code -> Maybe Price
-total items q s = fmap ((fromInteger q)*) $ fmap snd $ findItem items s
-
-findItem :: ItemList -> Code -> Maybe (Label,Price)
-findItem (It (code,item) items) s | code == s = Just item
-                                  | otherwise = findItem items s 
-findItem Nil _ = Nothing
-
-    
-itemMap :: (Item -> Item) -> ItemList -> ItemList
-itemMap f (It item items) = It (f item) (itemMap f items)
-itemMap _ Nil = Nil
+total :: [Item] -> Quantity -> Code -> Maybe Price
+total items q s = fmap ((fromInteger q)*) $ fmap snd $ lookup s items
 
