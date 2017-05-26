4cd093ee3d4e691b462cd4963d343102297589e1
ToF <cthibaut@octo.com>
2017-05-26 16:38:36 +0200

    applicative

--- Total.hs
+++ Total.hs
@@ -1,5 +1,6 @@
 module Total
 where
+import Data.Maybe
 
 type Price = Double
 type Quantity = Integer
@@ -8,11 +9,9 @@ type Code  = String
 type Item = (Code,(Label,Price))
 
 total :: [Item] -> String -> Code -> Maybe Price
-total items q s = case readQuantity q of
-    Just r -> fmap ((fromInteger r)*) $ fmap snd $ lookup s items
-    Nothing -> Nothing
+total items q s = j $ fmap (\f -> fmap f (fmap fromInteger (readQuantity q))) (fmap (*) (fmap snd (lookup s items)))
+    where j (Just a) = a
+          j Nothing  = Nothing
 
 readQuantity :: String -> Maybe Quantity
-readQuantity s = case reads s of
-    [(q,_)] -> Just q
-    []      -> Nothing
+readQuantity = fmap fst . listToMaybe . reads
