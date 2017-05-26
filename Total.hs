45537fac3dc5f983c39d6a479438ce158ff6939b
ToF <cthibaut@octo.com>
2017-05-26 14:23:30 +0200

    with signatures

--- Total.hs
+++ Total.hs
@@ -2,6 +2,7 @@ module Total
 where
 
 type Price = Double
+type Quantity = Integer
 type Label = String
 type Code  = String
 type Item = (Code,(Label,Price))
@@ -15,16 +16,20 @@ data Result a = Result a
     deriving (Show, Eq)
 
 
+total :: ItemList -> Quantity -> Code -> Result Price
 total items q s = resultMap ((fromInteger q)*) $ resultMap snd $ findItem items s
 
+findItem :: ItemList -> Code -> Result (Label,Price)
 findItem (It (code,item) items) s | code == s = Result item
                                   | otherwise = findItem items s 
 findItem Nil _ = NoResult
 
     
+resultMap :: (a -> b) -> Result a -> Result b
 resultMap f (Result x) = Result $ f x
 resultMap f NoResult   = NoResult
 
+itemMap :: (Item -> Item) -> ItemList -> ItemList
 itemMap f (It item items) = It (f item) (itemMap f items)
 itemMap _ Nil = Nil
 
